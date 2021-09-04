040415886bf1bc080048c58501ba597fd23a51ea
unknown <yuta.yanagihara@gmail.com>
2021-09-04 17:30:11 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import datetime
+import pandas as pd
+#import time
+#6行目は写真挿入などの時に使うかも
+#import streamlit.components.v1 as stc
+while(True):
+    i = 50
+    break
+st.title('混雑状況確認アプリ')
+
+st.write('始めに入退室ボタンを押してください')
+
+#checkbox?
+left_column,right_column = st.columns(2)
+left_button = left_column.button('入室')
+right_button = right_column.button('退室')
+
+if left_button:
+    st.write('iniadへようこそ')
+if right_button:
+    st.write('またのご利用お待ちしております。')
+
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+if left_button:
+    if i >= 30 and i <= 49:
+        latest_iteration.text(f'現在の施設内の人数 {i+1}')
+        st.write('人が若干増えてきました')
+        bar.progress(i+1)
+    if i >= 50 and i <= 79:
+        latest_iteration.text(f'現在の施設内の人数 {i+1}')
+        st.write('建物内で出来るだけ混雑を作らないようにしましょう')
+        bar.progress(i+1)
+    if i >= 80:
+        latest_iteration.text(f'現在の施設内の人数 {i+1}')
+        st.write('かなり人流が増えてきています')
+        bar.progress(i+1)
+    if i <= 29:
+        latest_iteration.text(f'現在の施設内の人数 {i+1}')
+        st.write('比較的空いています')
+        bar.progress(i+1)
+
+
+        
+if right_button:
+    i+=1
+    latest_iteration.text(f'現在の施設内の人数 {i-1}')
+    bar.progress(i-1)
+
+now = datetime.datetime.now()
+st.write(now.strftime('現在の日時は、%Y年%m月%d日 %H:%M:%S'))
+
+#この施設の混雑度具合はアンケートを取る
+#時間ごとに混んでいる施設をアドバイス
+if now.hour != 12 and now.hour != 13:
+    st.write('現在の時間帯は比較的どこの施設も空いています。')
+
+if now.hour >= 12 and now.hour <= 13:
+    st.write('この時間帯は普段だと、食堂が若干混んでいます') 
+
+#ここもアンケートを取る(いまのところリアルタイムで混雑度収集は厳しそうだから)
+#時間ごとに表示するデータフレームを変える(if now.hour >= 12 みたいに)
+df = pd.DataFrame({
+    '場所':['INIADホール','講義室','糖朝'],
+    '混雑度(MAXが100)':[60,40,50]
+})     
+
+#dataframe or table
+st.dataframe(df.style.highlight_max(axis=0))
+
+
+
+st.title('INIADについての混雑状況のお問い合わせ')
+expander1 = st.expander('INIAD内でどの場所が一番混雑していますか？')
+expander1.write('INIADホール付近は授業の前後でたくさんの生徒が集まります。')
+expander2 = st.expander('食堂のすいている時間帯を教えてください')
+expander2.write('糖朝については2限と3限の間の時間帯以外は基本的に空いています。その時間帯を狙いましょう。')
+expander3 = st.expander('表の見方を教えてください')
+expander3.write('1列目が場所、2列目がその場所の混雑度を表しています。列名のところをクリックすると、ソートもできるのでランキング形式にしてみるとさらに見やすくなります。')
+
+
+#stc.html('<img width="200" alt="test" src="https://cafe-mickey.com/coffee-life/wp-content/uploads/2021/02/image.gif">')
+
+
+
+
+
