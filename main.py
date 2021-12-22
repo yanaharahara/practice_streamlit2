1d523a2be45ce71caa746f04e2abcf5d06be301e
unknown <yuta.yanagihara@gmail.com>
2021-12-22 16:19:09 +0900

    add new function

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
-#音声認識ありかも(目の不自由な人に音声で混雑状況を伝える)
 import streamlit as st
 import datetime
 import pandas as pd
 import pytz
-#import winsound
-#import time
-#9行目は写真挿入などの時に使うかも
-#import streamlit.components.v1 as stc
 from PIL import Image
-
-if "hoge" not in st.session_state:
-   st.session_state.hoge = 0
-
-
-# import pandas as pd
-# df = pd.read_csv("sample.csv")
-# df["data"][0] += 1
-# df.to_csv("sample.csv", index = False)
-# print(df)  
-# st.write(df) 
-
-
-
-#バルーン
-#st.balloons()
-
 import sqlite3
+
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
+
+if "fuga" not in st.session_state:
+    st.session_state.fuga = False
+    c.execute('update counter set num = num + 1')
+    conn.commit()
+    c.execute('select num from counter')
+    number = c.fetchall()
+    st.session_state.hoge = number[0][0]
+    st.balloons()
+
 #c.execute('CREATE TABLE IF NOT EXISTS counter(num INTEGER)')
 #total = 0
 #c.execute('INSERT INTO counter(num) VALUES (?)', (total,))
 #conn.commit()
-c.execute('update counter set num = num + 1')
-conn.commit()
-c.execute('select num from counter')
-number = c.fetchall()
-st.write(number[0][0]) 
+
 
 def color_background(x):
             color = ""
@@ -47,75 +32,76 @@ def color_background(x):
             
             return [color for _ in x]
 
+def color_background_eng(x):
+            color = ""
+            if x["Congestion (Max=100)"] >= 80:
+                color = "background-color: #FF6666; color: #FFFFFF;"
+            elif x["Congestion (Max=100)"] <= 10:
+                color = "background-color: #659AD2; color: #FFFFFF;"
+            
+            return [color for _ in x]        
+
+
 
 def english():
     #ロゴの挿入
     image = Image.open('app_logo.png')
     st.image(image,use_column_width=True)
 
-    #st.title('混雑状況確認アプリ')
+    
+    st.write('Please press the Exit button when you exit.')
 
-    st.write('First, please press the Enter / Exit button.')
-
-    #checkbox?
     left_column,right_column = st.columns(2)
-    left_button = left_column.button('Entry')
     right_button = right_column.button('Exit')
-
-    if left_button:
-        st.write('Welcome to INIAD')
-        
-
-    if right_button:
-        st.write('We look forward to seeing you again.')
 
 
     latest_iteration = st.empty()
     bar = st.progress(0)
 
-    if left_button:
-        st.session_state.hoge += 1
-        if st.session_state.hoge >= 30 and st.session_state.hoge <= 49:
-            latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
-            st.write('The number of people has increased slightly')
-            bar.progress(st.session_state.hoge)
-        if st.session_state.hoge >= 50 and st.session_state.hoge <= 79:
-            latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
-            st.write('Try to create as little congestion as possible in the building.')
-            bar.progress(st.session_state.hoge)
-        if st.session_state.hoge >= 80:
-            latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
-            st.write('The flow of people is increasing considerably.')
-            bar.progress(st.session_state.hoge)
-        if st.session_state.hoge <= 29:
-            latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
-            st.write('It is relatively empty.')
-            bar.progress(st.session_state.hoge)
+    
+    if st.session_state.hoge >= 30 and st.session_state.hoge <= 49:
+        latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
+        st.write('The number of people has increased slightly')
+        bar.progress(st.session_state.hoge)
+    if st.session_state.hoge >= 50 and st.session_state.hoge <= 79:
+        latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
+        st.write('Try to create as little congestion as possible in the building.')
+        bar.progress(st.session_state.hoge)
+    if st.session_state.hoge >= 80:
+        latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
+        st.write('The flow of people is increasing considerably.')
+        bar.progress(st.session_state.hoge)
+    if st.session_state.hoge <= 29:
+        latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
+        st.write('It is relatively empty.')
+        bar.progress(st.session_state.hoge)
 
 
             
-    if right_button:
-        if st.session_state.hoge >= 1: 
-            st.session_state.hoge -= 1
+    if right_button and st.session_state.fuga == False:
+        st.session_state.fuga = True
+        if st.session_state.hoge >= 1:
+            c.execute('update counter set num = num - 1')
+            conn.commit()
+            c.execute('select num from counter')
+            number = c.fetchall()
+            st.session_state.hoge = number[0][0]
         latest_iteration.text(f'Current number of people in the facility {st.session_state.hoge}')
         bar.progress(st.session_state.hoge)
 
-    #now = datetime.datetime.now()
-    #st.write(now.strftime('現在の日時は、%Y年%m月%d日 %H:%M:%S'))
     now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
     st.write(now.strftime('The current date and time are %Y/%m/%d %H:%M:%S'))
 
 
     #時間ごとに混んでいる施設をアドバイス
     #時間ごとに表示するデータフレームを変える(if now.hour >= 12 みたいに)
-    #dataframe or table
     if now.hour == 12:
         st.write('Currently, the cafeteria and Totyo tend to be crowded.')
         df = pd.DataFrame({
         'Place':['B:Lecture room','F:Totyo','C:INIAD Hall','E:Cafeteria','D:Presentation Hub','A:Media Center'],
         'Congestion (Max=100)':[10,100,30,100,60,10]
         })
-        st.dataframe(df.style.apply(color_background,axis=1),width=10000,height=10000)
+        st.dataframe(df.style.apply(color_background_eng,axis=1),width=10000,height=10000)
 
     elif now.hour >= 8 and now.hour <= 11:
         st.write('Currently, the presentation hub on the first floor tends to be crowded')
@@ -123,7 +109,7 @@ def english():
         'Place':['C:Lecture room','D:Totyo','B:INIAD Hall','E:Cafeteria','F:Presentation Hub','A:Media Center'],
         'Congestion (Max=100)':[40,90,10,90,100,10]
         })
-        st.dataframe(df.style.apply(color_background,axis=1),width=10000,height=10000)
+        st.dataframe(df.style.apply(color_background_eng,axis=1),width=10000,height=10000)
         
 
     elif now.hour >= 13  and now.hour <= 17:
@@ -132,7 +118,7 @@ def english():
         'Place':['C:Lecture room','D:Totyo','B:INIAD Hall','F:Cafeteria','E:Presentation Hub','A:Media Center'],
         'Congestion (Max=100)':[15,50,10,100,70,5]
         })
-        st.dataframe(df.style.apply(color_background,axis=1),width=10000,height=10000)
+        st.dataframe(df.style.apply(color_background_eng,axis=1),width=10000,height=10000)
         
     elif now.hour >= 18  and now.hour <= 19:
         st.write('The presentation hub on the first floor tends to be crowded at this time of day  \nThe other facilities are relatively empty.')
@@ -140,28 +126,23 @@ def english():
         'Place':['C:Lecture room','D:Totyo','B:INIAD Hall','E:Cafeteria','F:Presentation Hub','A:Media Center'],
         'Congestion (Max=100)':[10,40,10,80,100,5]
         })
-        st.dataframe(df.style.apply(color_background,axis=1),width=10000,height=10000)
+        st.dataframe(df.style.apply(color_background_eng,axis=1),width=10000,height=10000)
 
     else:
         st.write('Currently, it is out of service hours. Entry hours are from 8:30am to 8:00pm on weekdays.')
         
-
-
-
-
-
-
-
+    
     st.title('Inquire about congestion at INIAD')
     expander1 = st.expander('Which areas in INIAD are the most crowded?')
     expander1.write('It depends on the time of day, but basically the cafeteria will be the most likely place to be crowded.')
     expander2 = st.expander('What is the best time to go to Totyo?')
     expander2.write('Except for the period between 2nd and 3rd period, Totyo is basically empty. Aim for those times.')
     expander3 = st.expander('How to read the table?')
-    expander3.write('The first column shows the location and the second column shows the congestion level of the location.  \nIf you click on the column name, you can also sort it, so it will be even easier to see it in a ranking format.')
+    expander3.write('The first column shows the location and the second column shows the congestion level of the location.  \nIf you click on the column name, you can also sort it, so it will be even easier to see it in a ranking format.\nIf the congestion level is above 80, the location will be shown in red, and if it is below 10, the location will be shown in blue.')
+    expander4 = st.expander('About congestion figures')
+    expander4.write('The congestion level of the most crowded place at the current time is set to 100. The other locations are calculated based on the results of comparing them to the location with the largest congestion level.')
 
     #リンクの設定(ここに建物のホームページのリンクを貼る)
-    st.write('INIAD Website')
     st.markdown('<a href="https://www.iniad.org/">INIAD Website</a>',unsafe_allow_html=True)
 
 def japanese():
@@ -169,62 +150,53 @@ def japanese():
     image = Image.open('app_logo.png')
     st.image(image,use_column_width=True)
 
-    #st.title('混雑状況確認アプリ')
+    st.write('退室の際に退室ボタンを押してください')
 
-    st.write('始めに入退室ボタンを押してください')
-
-    #checkbox?
     left_column,right_column = st.columns(2)
-    left_button = left_column.button('入室')
-    right_button = right_column.button('退室')
-
-    if left_button:
-        st.write('iniadへようこそ')
-        
-
-    if right_button:
-        st.write('またのご利用お待ちしております。')
-
-
+    right_button = right_column.button('退室')    
+    
     latest_iteration = st.empty()
     bar = st.progress(0)
 
-    if left_button:
-        st.session_state.hoge += 1
-        if st.session_state.hoge >= 30 and st.session_state.hoge <= 49:
-            latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
-            st.write('人が若干増えてきました')
-            bar.progress(st.session_state.hoge)
-        if st.session_state.hoge >= 50 and st.session_state.hoge <= 79:
-            latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
-            st.write('建物内で出来るだけ混雑を作らないようにしましょう')
-            bar.progress(st.session_state.hoge)
-        if st.session_state.hoge >= 80:
-            latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
-            st.write('かなり人流が増えてきています')
-            bar.progress(st.session_state.hoge)
-        if st.session_state.hoge <= 29:
-            latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
-            st.write('比較的空いています')
-            bar.progress(st.session_state.hoge)
+    
+    if st.session_state.hoge >= 30 and st.session_state.hoge <= 49:
+        latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
+        st.write('人が若干増えてきました')
+        bar.progress(st.session_state.hoge)
+    if st.session_state.hoge >= 50 and st.session_state.hoge <= 79:
+        latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
+        st.write('建物内で出来るだけ混雑を作らないようにしましょう')
+        bar.progress(st.session_state.hoge)
+    if st.session_state.hoge >= 80:
+        latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
+        st.write('かなり人流が増えてきています')
+        bar.progress(st.session_state.hoge)
+    if st.session_state.hoge <= 29:
+        latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
+        st.write('比較的空いています')
+        bar.progress(st.session_state.hoge)
 
 
             
-    if right_button:
+    if right_button and st.session_state.fuga == False:
+        st.session_state.fuga = True
         if st.session_state.hoge >= 1:
-            st.session_state.hoge -= 1
+            c.execute('update counter set num = num - 1')
+            conn.commit()
+            c.execute('select num from counter')
+            number = c.fetchall()
+            st.session_state.hoge = number[0][0]
+        
         latest_iteration.text(f'現在の施設内の人数 {st.session_state.hoge}')
         bar.progress(st.session_state.hoge)
 
-    #now = datetime.datetime.now()
-    #st.write(now.strftime('現在の日時は、%Y年%m月%d日 %H:%M:%S'))
+    
     now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
     st.write(now.strftime('現在の日時は、%Y年%m月%d日 %H:%M:%S'))
 
 
     #時間ごとに混んでいる施設をアドバイス
     #時間ごとに表示するデータフレームを変える(if now.hour >= 12 みたいに)
-    #dataframe or table
     if now.hour == 12:
         st.write('現在の時間帯は食堂と糖朝が混雑する傾向にあります')
         df = pd.DataFrame({
@@ -249,8 +221,7 @@ def japanese():
         })
         
         st.dataframe(df.style.apply(color_background,axis=1),width=10000,height=10000)
-        #st.dataframe(df.style.highlight_max(axis=0,color="#FF6666").highlight_min(axis=0,color="#659AD2"),width=10000,height=10000)
-        #color = "background-color: #FFFFFF; color: #000000;"
+        
         
     elif now.hour >= 18  and now.hour <= 19:
         st.write('現在の時間帯は1階のプレゼンテーションハブが混雑する傾向にあります  \nまた、ほかの施設は比較的空いています')
@@ -293,28 +264,6 @@ def japanese():
         #time.sleep(5)
     #st.success('Done!') 
     
-    #音声の再生
-    #st.audio('音声ファイルの名前')
-
-    #音声の自動再生(この方法だとボタンを押す度に反応してしまう)
-    #import winsound
-    #winsound.PlaySound('音声ファイルの名前.wav', winsound.SND_FILENAME)
-
-    #音声の自動再生方法2
-    #from fabric.api import runs_once
-    #@runs_once
-    #def play():
-        #winsound.PlaySound('新音声データ.wav', winsound.SND_FILENAME)
-    #def play2():
-        #play()
-    #play2()
-
-
-
-
-
-    #stc.html('<img width="200" alt="test" src="https://cafe-mickey.com/coffee-life/wp-content/uploads/2021/02/image.gif">')
-
 
 lang = st.selectbox("言語を選択してください。please choose language",("日本語","English"))
 
@@ -323,7 +272,6 @@ if lang == "日本語":
     japanese()
 elif lang == "English":
     english()
-
 else:
     japanese()
 
